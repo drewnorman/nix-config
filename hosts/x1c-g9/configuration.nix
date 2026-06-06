118c1ca2cfa0ae3ed0422a84c5d4f2083ce3e3cf
Drew Norman <drewnorman739@gmail.com>
2026-06-05 20:15:27 -0600

    fix(greetd): remember last login user

--- hosts/x1c-g9/configuration.nix
+++ hosts/x1c-g9/configuration.nix
@@ -59,7 +59,7 @@
     greetd = {
       enable = true;
       settings.default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd sway";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd sway";
         user = "greeter";
       };
     };
